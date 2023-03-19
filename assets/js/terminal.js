58c2416f38db35a769cfe1d34f38a283e5ff8391
Drian <11167300+fdrian@users.noreply.github.com>
2023-03-19 05:14:28 -0300

    refactor

--- assets/js/terminal.js
+++
@@ -1,6 +0,0 @@
- var typing = new Typed (".animate",{
-      strings:['Hello World!','My blood is made of coffee', 'Are you a one or zero?', 'Keep It Simple, Stupid!'],
-      typeSpeed:120,
-      backSpeed:70,
-      loop:true,
-   });
